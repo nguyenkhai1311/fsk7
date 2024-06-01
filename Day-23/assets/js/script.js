25ac965a0a9c7d77c30c1c4a0cd6e6230c4455ee
Khải Nguyễn <khainh@fullstack.edu.vn>
2024-06-01 23:47:07 +0700

    Update tab Login

--- Day-23/assets/js/script.js
+++ Day-23/assets/js/script.js
@@ -3,8 +3,6 @@ const tabSignup = document.querySelector('.tabSignup');
 const tabLogin = document.querySelector('.tabLogin');
 const formLogin = document.querySelector('.formLogin');
 const formSignup = document.querySelector('.formSignup');
-const inputEmail = document.querySelectorAll('.inputEmail');
-const inputPassword = document.querySelectorAll('.inputPassword');
 const error = document.querySelectorAll('.error');
 const errorEmailInfo = document.querySelector('.errorEmailInfo');
 const errorEmailFormat = document.querySelector('.errorEmailFormat');
@@ -14,6 +12,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const loginEmail = document.querySelector('.loginEmail');
 const passwordEmail = document.querySelector('.passwordEmail');
+const btnSignup = document.querySelector('.btnSignup');
+const iconEyeSlash = document.querySelector('.iconEyeSlash');
+const iconEye = document.querySelector('.iconEye');
 
 login.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -25,6 +26,26 @@ overlay.addEventListener('click', () => {
 
 iconClose.addEventListener('click', () => {
     modal.style.display = 'none';
+});
+
+iconEyeSlash.addEventListener('click', () => {
+    iconEyeSlash.style.display = 'none';
+    iconEye.style.display = 'block';
+    if (passwordEmail.type === 'password') {
+        passwordEmail.type = 'text';
+    } else {
+        passwordEmail.type = 'password';
+    }
+});
+
+iconEye.addEventListener('click', () => {
+    iconEye.style.display = 'none';
+    iconEyeSlash.style.display = 'block';
+    if (passwordEmail.type === 'password') {
+        passwordEmail.type = 'text';
+    } else {
+        passwordEmail.type = 'password';
+    }
 });
 
 tabSignup.addEventListener('click', () => {
@@ -51,139 +72,120 @@ tabLogin.addEventListener('click', () => {
     });
 });
 
-inputEmail.forEach((input) => {
-    input.addEventListener('blur', () => {
-        if (input.value === '') {
-            input.style.borderColor = 'red';
-            inputPassword.forEach((input) => {
-                input.style.borderColor = 'red';
-            });
+loginEmail.addEventListener('blur', () => {
+    if (loginEmail.value === '') {
+        loginEmail.style.borderColor = 'red';
+        if (passwordEmail.value === '') {
+            passwordEmail.style.borderColor = 'red';
             error.forEach((error) => {
                 errorEmailInfo.style.display = 'block';
                 errorPasswordInfo.style.display = 'block';
                 errorEmailFormat.style.display = 'none';
             });
         } else {
-            if (!validateEmail(input.value)) {
-                error.forEach((error) => {
-                    errorEmailInfo.style.display = 'none';
-                    errorEmailFormat.style.display = 'block';
-                });
-            } else {
-                input.style.borderColor = 'green';
-                error.forEach((error) => {
-                    errorEmailInfo.style.display = 'none';
-                    errorEmailFormat.style.display = 'none';
-                });
-            }
-        }
-    });
-
-    input.addEventListener('keyup', () => {
-        console.log(input.value);
-        if (input.value === '') {
+            passwordEmail.style.borderColor = 'green';
             error.forEach((error) => {
                 errorEmailInfo.style.display = 'block';
-                errorEmailFormat.style.display = 'none';
-            });
-        } else {
-            if (!validateEmail(input.value)) {
-                error.forEach((error) => {
-                    errorEmailInfo.style.display = 'none';
-                    errorEmailFormat.style.display = 'block';
-                });
-            } else {
-                error.forEach((error) => {
-                    errorEmailInfo.style.display = 'none';
-                    errorEmailFormat.style.display = 'none';
-                });
-            }
-        }
-    });
-});
-
-inputPassword.forEach((input) => {
-    input.addEventListener('blur', () => {
-        if (input.value === '') {
-            input.style.borderColor = 'red';
-            inputEmail.forEach((inputEmail) => {
-                if (loginEmail.value === '') {
-                    console.log(123);
-                    input.style.borderColor = 'red';
-                    error.forEach((error) => {
-                        errorEmailInfo.style.display = 'block';
-                        errorPasswordInfo.style.display = 'block';
-                        errorEmailFormat.style.display = 'none';
-                    });
-                }
-            });
-        } else {
-            input.style.borderColor = 'green';
-        }
-    });
-
-    input.addEventListener('keyup', () => {
-        if (input.value !== '') {
-            error.forEach((error) => {
                 errorPasswordInfo.style.display = 'none';
-            });
-        } else {
-            error.forEach((error) => {
-                errorPasswordInfo.style.display = 'block';
+                errorEmailFormat.style.display = 'none';
             });
         }
-    });
-});
-
-formLogin.addEventListener('submit', (e) => {
-    inputEmail.forEach((inputEmail) => {
-        if (loginEmail.value === '') {
-            e.preventDefault();
-            inputEmail.style.borderColor = 'red';
-            inputPassword.forEach((input) => {
-                if (passwordEmail.value === '') {
-                    input.style.borderColor = 'red';
-                    error.forEach((error) => {
-                        errorEmailInfo.style.display = 'block';
-                        errorPasswordInfo.style.display = 'block';
-                        errorEmailFormat.style.display = 'none';
-                    });
-                } else {
-                    input.style.borderColor = 'green';
-                    error.forEach((error) => {
-                        errorEmailInfo.style.display = 'block';
-                        errorPasswordInfo.style.display = 'none';
-                        errorEmailFormat.style.display = 'none';
-                    });
-                }
-            });
-        }
+    } else {
         if (!validateEmail(loginEmail.value)) {
-            e.preventDefault();
-            inputEmail.style.borderColor = 'red';
             error.forEach((error) => {
                 errorEmailInfo.style.display = 'none';
                 errorEmailFormat.style.display = 'block';
             });
+            if (passwordEmail.value === '') {
+                passwordEmail.style.borderColor = 'red';
+                error.forEach((error) => {
+                    errorPasswordInfo.style.display = 'block';
+                });
+            }
+        } else {
+            loginEmail.style.borderColor = 'green';
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'none';
+                errorEmailFormat.style.display = 'none';
+            });
         }
-    });
+    }
+});
 
-    // inputPassword.forEach((input) => {
-    //     if (input.value === '') {
-    //         input.style.borderColor = 'red';
-    //         inputEmail.forEach((inputEmail) => {
-    //             if (inputEmail.value === '') {
-    //                 input.style.borderColor = 'red';
-    //                 error.forEach((error) => {
-    //                     errorEmailInfo.style.display = 'block';
-    //                     errorPasswordInfo.style.display = 'block';
-    //                     errorEmailFormat.style.display = 'none';
-    //                 });
-    //             }
-    //         });
-    //     } else {
-    //         input.style.borderColor = 'green';
-    //         errorPasswordInfo.style.display = 'block';
-    //     }
-    // });
+loginEmail.addEventListener('keyup', () => {
+    if (loginEmail.value === '') {
+        error.forEach((error) => {
+            errorEmailInfo.style.display = 'block';
+            errorEmailFormat.style.display = 'none';
+        });
+    } else {
+        if (!validateEmail(loginEmail.value)) {
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'none';
+                errorEmailFormat.style.display = 'block';
+            });
+        } else {
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'none';
+                errorEmailFormat.style.display = 'none';
+            });
+        }
+    }
+});
+
+passwordEmail.addEventListener('blur', () => {
+    if (passwordEmail.value === '') {
+        passwordEmail.style.borderColor = 'red';
+        if (loginEmail.value === '') {
+            loginEmail.style.borderColor = 'red';
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'block';
+                errorPasswordInfo.style.display = 'block';
+                errorEmailFormat.style.display = 'none';
+            });
+        }
+    } else {
+        passwordEmail.style.borderColor = 'green';
+    }
+});
+
+passwordEmail.addEventListener('keyup', () => {
+    if (passwordEmail.value !== '') {
+        error.forEach((error) => {
+            errorPasswordInfo.style.display = 'none';
+        });
+    } else {
+        error.forEach((error) => {
+            errorPasswordInfo.style.display = 'block';
+        });
+    }
+});
+
+formLogin.addEventListener('submit', (e) => {
+    if (loginEmail.value === '') {
+        e.preventDefault();
+        loginEmail.style.borderColor = 'red';
+        if (passwordEmail.value === '') {
+            passwordEmail.style.borderColor = 'red';
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'block';
+                errorPasswordInfo.style.display = 'block';
+                errorEmailFormat.style.display = 'none';
+            });
+        } else {
+            passwordEmail.style.borderColor = 'green';
+            error.forEach((error) => {
+                errorEmailInfo.style.display = 'block';
+                errorPasswordInfo.style.display = 'none';
+                errorEmailFormat.style.display = 'none';
+            });
+        }
+    } else if (!validateEmail(loginEmail.value)) {
+        e.preventDefault();
+        loginEmail.style.borderColor = 'red';
+        error.forEach((error) => {
+            errorEmailInfo.style.display = 'none';
+            errorEmailFormat.style.display = 'block';
+        });
+    }
 });
