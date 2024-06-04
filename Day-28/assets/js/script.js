69b7b255eb869a03f6c13c266ef8a850c76a97e7
Khải Nguyễn <khainh@fullstack.edu.vn>
2024-06-05 01:46:49 +0700

    Update add task & delete task

--- Day-28/assets/js/script.js
+++ Day-28/assets/js/script.js
@@ -19,14 +19,74 @@ todoBtn.addEventListener('click', function (e) {
                         </div>
                     </div>
                 </div>`;
-
+    addTodo.push({
+        count: `item-${count}`,
+        value: inputValue,
+    });
     count++;
     listItem.innerHTML += html;
     todoInput.value = '';
+    console.log(addTodo);
 });
 
 listItem.addEventListener('click', function (e) {
     if (e.target.classList.contains('fa-edit')) {
-        console.log(e.target.outerHTML);
+        console.log(
+            addTodo.filter(
+                (item) =>
+                    item.count ===
+                    e.target.parentElement.parentElement.parentElement
+                        .children[0].classList[0]
+            )
+        );
+        e.target.parentElement.parentElement.parentElement.outerHTML = `<form action="#!">
+        <input
+            type="text"
+            placeholder="Update task"
+            class="todoInputAdd"
+            value="${
+                addTodo.filter(
+                    (item) =>
+                        item.count ===
+                        e.target.parentElement.parentElement.parentElement
+                            .children[0].classList[0]
+                )[0].value
+            }"
+        />
+        <button type="button" class="todoButton">Add Task</button>
+    </form>
+    `;
+        var todoInputAdd = document.querySelector('.todoInputAdd');
+
+        todoInputAdd.addEventListener('change', function (event) {
+            addTodo.map((item) => {
+                console.log(
+                    item.count ===
+                        e.target.parentElement.parentElement.parentElement
+                            .children[0].classList[0]
+                );
+                if (
+                    item.count ===
+                    e.target.parentElement.parentElement.parentElement
+                        .children[0].classList[0]
+                ) {
+                    item.value = e.target.value;
+                }
+                return item;
+            });
+
+            console.log(addTodo);
+        });
+    } else if (e.target.classList.contains('fa-trash')) {
+        var isYes = confirm('Bạn có chắc chắn muốn xóa không?');
+        if (isYes) {
+            e.target.parentElement.parentElement.parentElement.remove();
+            addTodo = addTodo.filter(
+                (item) =>
+                    item.count !==
+                    e.target.parentElement.parentElement.parentElement
+                        .children[0].classList[0].split
+            );
+        }
     }
 });
