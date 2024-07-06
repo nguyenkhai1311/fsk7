4c713ad29701ea1155c660d1440dda1781bf0bc7
Khải Nguyễn <khainh@fullstack.edu.vn>
2024-07-07 01:08:32 +0700

    Fix class F8

--- Day-37/component.js
+++ Day-37/component.js
@@ -1,51 +1,68 @@
-class F8 extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
+class F8 {
+    static components = {};
 
-    static component(name, { data, template }) {
-        const templateEl = document.createElement('template');
-        templateEl.innerHTML = template;
-        const templateNode = templateEl.content.cloneNode(true);
+    static component(name, options) {
+        this.components[name] = options;
+        customElements.define(
+            name,
+            class extends HTMLElement {
+                constructor() {
+                    super();
+                    this.attachShadow({ mode: 'open' });
+                    this.state = options.data ? options.data() : {};
+                    this.render();
+                }
 
-        class Component extends F8 {
-            constructor() {
-                super();
-                this.state = data ? data() : {};
+                render() {
+                    const template = document.createElement('template');
+                    template.innerHTML = options.template.replace(
+                        /\{\{(.*?)\}\}/g,
+                        (_, key) => {
+                            return this.state[key.trim()];
+                        }
+                    );
+                    this.shadowRoot.innerHTML = '';
+                    this.shadowRoot.appendChild(
+                        template.content.cloneNode(true)
+                    );
 
-                // this.template = template;
-                this.render();
+                    this.addEventListeners();
+                }
+
+                addEventListeners() {
+                    const buttons = this.shadowRoot.querySelectorAll('button');
+                    buttons.forEach((button) => {
+                        if (button.hasAttribute('v-on:click')) {
+                            button.addEventListener('click', () => {
+                                const method =
+                                    button.getAttribute('v-on:click');
+                                this[method]();
+                                this.render();
+                            });
+                        }
+                        if (button.hasAttribute('v-on:dblclick')) {
+                            button.addEventListener('dblclick', () => {
+                                const method =
+                                    button.getAttribute('v-on:dblclick');
+                                this[method]();
+                                this.render();
+                            });
+                        }
+                    });
+                }
+
+                ['count++']() {
+                    this.state.count++;
+                }
+
+                ['count--']() {
+                    this.state.count--;
+                }
+
+                ['title="Hello F8"']() {
+                    this.state.title = 'Hello F8';
+                }
             }
-
-            render() {
-                document.body.appendChild(templateNode);
-                // templateEl.innerHTML = this.template
-                //     .replace(/{{\s*title\s*}}/g, this.state.title)
-                //     .replace(/{{\s*count\s*}}/g, this.state.count);
-                // this.shadowRoot.innerHTML = this.template
-                //     .replace(/{{\s*title\s*}}/g, this.state.title)
-                //     .replace(/{{\s*count\s*}}/g, this.state.count);
-                // this.shadowRoot
-                //     .querySelector('button:nth-child(3)')
-                //     .addEventListener('click', () => {
-                //         this.state.count--;
-                //         this.update();
-                //     });
-                // this.shadowRoot
-                //     .querySelector('button:nth-child(4)')
-                //     .addEventListener('click', () => {
-                //         this.state.count++;
-                //         this.update();
-                //     });
-            }
-
-            update() {
-                this.shadowRoot.querySelector('h2').textContent =
-                    this.state.count;
-            }
-        }
-
-        customElements.define(name, Component);
+        );
     }
 }
